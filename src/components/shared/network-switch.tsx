ec367f4fcf094ac85171f79d44d8649d63a57968
0x6A6471 <hello@jakequinter.io>
2025-11-18 11:55:55 -0600

    feat: add opts support to fromHex & fromBase64

--- src/components/shared/network-switch.tsx
+++ src/components/shared/network-switch.tsx
@@ -1,19 +1,22 @@
+import { networks } from "bitcoinjs-lib";
+
 import Label from "@/components/ui/label";
 import Switch from "@/components/ui/switch";
 import useDisplay from "@/hooks/useDisplay";
 
 export default function NetworkSwitch() {
 	const { network, toggleNetwork } = useDisplay();
+	const isMainnet = network === networks.bitcoin;
 
 	return (
 		<div className="flex items-center space-x-2">
 			<Switch
 				id="network"
-				checked={network === "mainnet"}
+				checked={isMainnet}
 				onCheckedChange={toggleNetwork}
 			/>
 			<Label htmlFor="network" className="w-14 text-right font-normal">
-				{network === "mainnet" ? "Mainnet" : "Testnet"}
+				{isMainnet ? "Mainnet" : "Testnet"}
 			</Label>
 		</div>
 	);
